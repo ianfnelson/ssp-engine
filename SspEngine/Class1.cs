a8ab49d6600a22bbfa498e2a5ee33027176f04a6
Ian Nelson <ian@ianfnelson.com>
2014-06-21 20:04:19 +0100

    initial commit

---
+++ SspEngine/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SspEngine
+{
+    public class Class1
+    {
+    }
+}
